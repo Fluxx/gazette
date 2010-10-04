3aa8baa515b1840068e0d7784e7aec3213cc40b1
Jeff Pollard <jeff.pollard@gmail.com>
2010-10-03 18:23:26 -0700

    Move client spec over to its own folder and broke up the spec

---
+++ spec/client/authenticate_spec.rb
@@ -0,0 +1,78 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe Gazette::Client, "#authenticate" do
+  before(:each) do
+    @client = Gazette::Client.new("foo")
+  end
+  
+  describe "HTTP basic auth" do
+    
+    def mock_get_request
+      @my_get = Net::HTTP::Get.new('/api/authenticate')
+      Net::HTTP::Get.stub!(:new).and_return(@my_get)
+    end
+    
+    before(:each) do
+      mock_get_request
+      stub_instapaper_api(:authenticate => {:status => 200})
+    end
+    
+    it "passes along the username" do
+      @my_get.should_receive(:basic_auth).with("foo", nil)
+      @client.authenticate
+    end
+    
+    it "passes along the password if specified" do
+      @client = Gazette::Client.new("foo", :password => "bar")
+      @my_get.should_receive(:basic_auth).with("foo", "bar")
+      @client.authenticate
+    end
+  end
+    
+  describe "with a 200 OK response" do
+    before(:each) do
+      stub_instapaper_api(:authenticate => {:status => 200})
+    end
+    
+    it "returns a Gazette::Response::Success object" do
+      @client.authenticate.should be_a Gazette::Response::Success
+    end
+  end
+  
+  describe "with a 403 response" do
+    before(:each) do
+      stub_instapaper_api(:authenticate => {:status => 403})
+    end
+    
+    it "rasises a Gazette::Response::InvalidCredentials" do
+      lambda { 
+        @client.authenticate 
+      }.should raise_error(Gazette::Response::InvalidCredentials)
+    end
+  end
+  
+  describe "with a 500 response" do
+    before(:each) do
+      stub_instapaper_api(:authenticate => {:status => 500})
+    end
+    
+    it "rasises a Gazette::Response::ServerError" do
+      lambda { 
+        @client.authenticate 
+      }.should raise_error(Gazette::Response::ServerError)
+    end
+  end
+  
+  describe "with a XXX response" do
+    before(:each) do
+      stub_instapaper_api(:authenticate => {:status => 999})
+    end
+    
+    it "rasises a Gazette::Response::UnknownError" do
+      lambda { 
+        @client.authenticate 
+      }.should raise_error(Gazette::Response::UnknownError)
+    end
+  end
+
+end
